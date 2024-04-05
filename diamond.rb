17ee158f0347762deb8d85450b9e7ed1bcfba81d
Logan Cole <emtlogancole@gmail.com>
2024-04-05 12:47:59 -0500

    pseudocode

--- diamond.rb
+++ diamond.rb
@@ -5,3 +5,10 @@ Write your code for the 'Diamond' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/diamond` directory.
 =end
+class Diamond
+  def self.make_diamond(letter)
+  # if it's a just return "A\n"
+  # start with the correct char line with one space in the middle
+  # count back toward A filling in letters and adding spaces before and after each letter
+  end
+end
