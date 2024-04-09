21c73ee88b77ffc01d8fe071e53142a7bdbdeca4
Logan Cole <emtlogancole@gmail.com>
2024-04-09 17:55:30 -0500

    getting warmer

--- diamond.rb
+++ diamond.rb
@@ -8,9 +8,14 @@ To get started with TDD, see the `README.md` file in your
 class Diamond
   def self.make_diamond(letter)
     # return for a only
+    return "A\n" if letter == 'A'
     # get array of all used letters
+    letters = ("A"..letter).to_a
+    back_half = letters[0..-2].reverse
     # get a string formatted like each line with appropriate char numbers
+    letters_together = (letters += back_half[0..-1]).join
     # create top half of diamond
+    diamond = letters.map {|letter| letters_together.gsub(/[^#{letter}]/, ' ') + "\n"}.join
     # create bottom half and combine
   end
 end
